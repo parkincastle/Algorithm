cdea18610eb3a210b83f478f0f7b0219c2bddb65
parkincastle <shfoemesmsrPwjd1@gmail.com>
2021-04-27 17:05:10 +0900

    Heap Sort

---
+++ Heap_Sort.c
@@ -0,0 +1,26 @@
+// heap(Tree) 완전이진 트리
+// 삽입 : 맨 마지막
+// 삭제 : root
+// 최대 : 부모 >= 자식
+// 최소 : 부코 <= 자식
+
+#include <stdio.h>
+#define SIZE 9
+
+int heap[SIZE] = { 9, 5, 8, 3, 2, 4, 6, 7, 1 };
+
+void heap_sort() {
+
+	if (2 * n + 2 != NULL) {
+
+	}
+	else {
+		printf("Heap정렬이 끝났습니다.");
+	}
+}
+
+
+int main() {
+
+}
+
